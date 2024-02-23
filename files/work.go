54778fe7f72d872fe556ef3530b0b5093c9d4aac
SunSung W541 <mail@embedded.biz.ua>
2024-02-23 19:45:46 +0100

    1

--- files/work.go
+++ files/work.go
@@ -1,7 +1,45 @@
 package files
 
-import "go.uber.org/zap"
+import (
+	"go.uber.org/zap"
+	"os"
+)
+
+type historyFallObj struct {
+	log *zap.Logger
+	dir string
+}
 
 func GO(log *zap.Logger) {
 	log.Info("Work from file")
+
+	//Инициализация обьекта
+	obj := historyFallObj{}
+	obj.dir = "./files/.history/"
+	obj.log = log
+
+	obj.writeFile()
+}
+
+// Запись данных в файл
+func (obj historyFallObj) writeFile() {
+	fileName := obj.dir + "output.txt"
+	data := "Пример данных для записи в файл."
+
+	// Открытие файла для записи, флаг os.O_WRONLY|os.O_CREATE|os.O_TRUNC указывает на то, что файл будет создан или перезаписан, если уже существует.
+	file, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		obj.log.Error("Не удалось открыть файл:", zap.Error(err))
+		return
+	}
+	defer file.Close()
+
+	// Запись данных в файл
+	_, err = file.WriteString(data)
+	if err != nil {
+		obj.log.Error("Ошибка записи данных в файл:", zap.Error(err))
+		return
+	}
+
+	obj.log.Info("Данные записаны в файл")
 }
