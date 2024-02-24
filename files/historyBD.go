a739b1cc50c18b76475c5389b2b274c33977d97d
SunSung W541 <mail@embedded.biz.ua>
2024-02-25 00:02:11 +0100

    обработка файла в базе базовая

--- files/historyBD.go
+++ files/historyBD.go
@@ -346,7 +346,6 @@ func (obj localSQLiteObj) addSHA(key string) uint32 {
 	}
 
 	tx := obj.BeginTransaction("addSHA")
-
 	obj.tapActivityTransaction(tx)
 
 	result, err := tx.Exec("INSERT INTO `sha` (`key`) VALUES (?)", key)
@@ -367,25 +366,102 @@ func (obj localSQLiteObj) addSHA(key string) uint32 {
 //.//
 
 // Поиск файла по названию
-func (obj localSQLiteObj) searchFile(fileName string) _historyFallFileObj {
+func (obj localSQLiteObj) searchFile(fileName string) (_historyFallFileObj, bool) {
 	file := _historyFallFileObj{}
-	return file
+	status := true
+
+	err := obj.db.QueryRow("SELECT `id`, `key`, `isDel`, `beginID` FROM `files` WHERE `key` = ?", fileName).Scan(
+		&file.id,
+		&file.key,
+		&file.isDel,
+		&file.begin,
+	)
+
+	if err != nil {
+		obj.log.Error("DB", zap.String("func", "searchFile"), zap.Error(err))
+		status = false
+	}
+
+	return file, status
 }
 
 // Выбор файла по ID
-func (obj localSQLiteObj) getFile(id uint32) _historyFallFileObj {
+func (obj localSQLiteObj) getFile(id uint32) (_historyFallFileObj, bool) {
 	file := _historyFallFileObj{}
-	return file
+	status := true
+
+	err := obj.db.QueryRow("SELECT `id`, `key`, `isDel`, `beginID` FROM `files` WHERE `id` = ?", id).Scan(
+		&file.id,
+		&file.key,
+		&file.isDel,
+		&file.begin,
+	)
+
+	if err != nil {
+		obj.log.Error("DB", zap.String("func", "searchFile"), zap.Error(err))
+		status = false
+	}
+
+	return file, status
+}
+
+// обновление записи по ID
+func (obj localSQLiteObj) updFile(id uint32, beginID uint32, isDel bool) {
+	tx := obj.BeginTransaction("updFile")
+	obj.tapActivityTransaction(tx)
+
+	_, err := tx.Exec("UPDATE `files` SET `isDel` = ?, `beginID` = ? WHERE `id` = ?;", isDel, beginID, id)
+	if err != nil {
+		tx.Rollback()
+		obj.log.Error("Break transaction", zap.String("func", "updFile"), zap.Error(err))
+	}
+
+	obj.EndTransaction(tx, "updFile")
 }
 
 // Добавление нового файла
 func (obj localSQLiteObj) addFile(name string, beginID uint32) uint32 {
-	return 0
+	fileObj, status := obj.searchFile(name)
+
+	//	Обработка если такой файл в базе
+	if status {
+		if fileObj.begin != beginID && fileObj.isDel { //	Обновление если повторно добавляется ранее уже добавленный и удаленный файл
+			obj.updFile(fileObj.id, beginID, false)
+		}
+
+		return fileObj.id
+	}
+
+	tx := obj.BeginTransaction("addFile")
+	obj.tapActivityTransaction(tx)
+
+	result, err := tx.Exec("INSERT INTO `files` (`key`, `isDel`, `beginID`) VALUES (?, true, ?)", name, beginID)
+	if err != nil {
+		tx.Rollback()
+		obj.log.Error("Break transaction", zap.String("func", "addFile"), zap.Error(err))
+	}
+
+	lastInsertID, err := result.LastInsertId()
+	if err != nil {
+		obj.log.Error("Break upload LastInsertId", zap.String("func", "addFile"), zap.Error(err))
+	}
+
+	obj.EndTransaction(tx, "addFile")
+	return uint32(lastInsertID)
 }
 
 // Управление статусом файла
-func (obj localSQLiteObj) isDelFile(isDelete bool) {
+func (obj localSQLiteObj) setDelFile(id uint32, isDelete bool) {
+	tx := obj.BeginTransaction("setDelFile")
+	obj.tapActivityTransaction(tx)
 
+	_, err := tx.Exec("UPDATE `files` SET `isDel` = ? WHERE `id` = ?;", isDelete, id)
+	if err != nil {
+		tx.Rollback()
+		obj.log.Error("Break transaction", zap.String("func", "setDelFile"), zap.Error(err))
+	}
+
+	obj.EndTransaction(tx, "setDelFile")
 }
 
 //.//
@@ -393,13 +469,17 @@ func (obj localSQLiteObj) isDelFile(isDelete bool) {
 // Поиск вектора по ключу наследования
 func (obj localSQLiteObj) searchVector(key string) (_historyFallVectorObj, bool) {
 	vector := _historyFallVectorObj{}
-	return vector, false
+	status := false
+
+	return vector, status
 }
 
 // Получение вектора по ID
 func (obj localSQLiteObj) getVector(id uint32) (_historyFallVectorObj, bool) {
 	vector := _historyFallVectorObj{}
-	return vector, false
+	status := false
+
+	return vector, status
 }
 
 // Добавление вектора
