ae1c9e18b2e68d1863568821d904aa58720557d6
SunSung W541 <mail@embedded.biz.ua>
2024-02-25 15:40:43 +0100

    * temp/db [1.0.04cf0bcf] :  автопривязка базы к IDE

--- pkg/module/historyConst.go
+++ pkg/module/historyConst.go
@@ -1,4 +1,4 @@
 package module
 
-const constVersionHistoryFall string = "1.0.716b89a0"
-const constDateUpdateHistoryFall = "02-25-2024"
+const constVersionHistoryFall string = "1.0.a6e416f0"
+const constDateUpdateHistoryFall string = "02-25-2024"
