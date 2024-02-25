bb11a2b7fedbd301c0769f223fb061c8fff5b5b2
SunSung W541 <mail@embedded.biz.ua>
2024-02-25 16:00:07 +0100

    * temp/db [1.0.74414f06] :  clear commit

--- pkg/module/historyConst.go
+++ pkg/module/historyConst.go
@@ -1,4 +1,4 @@
 package module
 
-const constVersionHistoryFall string = "1.0.b2cc2316"
+const constVersionHistoryFall string = "1.0.9ebeea19"
 const constDateUpdateHistoryFall string = "02-25-2024"
