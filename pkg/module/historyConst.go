0717fdea4fa1bd6a282338055059f561ce8dbbce
SunSung W541 <mail@embedded.biz.ua>
2024-02-26 00:31:39 +0100

    * temp/gln [1.0.371c24e7] :  обработка схождения для больших и маленьких файлов

--- pkg/module/historyConst.go
+++ pkg/module/historyConst.go
@@ -1,4 +1,4 @@
 package module
 
-const constVersionHistoryFall string = "1.0.91eb8c29"
+const constVersionHistoryFall string = "1.0.acefe7c9"
 const constDateUpdateHistoryFall string = "02-26-2024"
