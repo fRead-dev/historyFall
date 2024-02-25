3f91e7e40d91a0c037c42f894c2b8ed4b77a6e50
SunSung W541 <mail@embedded.biz.ua>
2024-02-25 16:00:18 +0100

    * temp/db [1.0.30574b19] :  add conf

--- pkg/module/historyConst.go
+++ pkg/module/historyConst.go
@@ -1,4 +1,4 @@
 package module
 
-const constVersionHistoryFall string = "1.0.9ebeea19"
+const constVersionHistoryFall string = "1.0.74414f06"
 const constDateUpdateHistoryFall string = "02-25-2024"
