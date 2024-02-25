87f7afa7c3989ee634a81137b037c792cbe1a271
SunSung W541 <mail@embedded.biz.ua>
2024-02-25 15:57:27 +0100

    * temp/db [1.0.b2cc2316] :  не стоило того

--- pkg/module/historyConst.go
+++ pkg/module/historyConst.go
@@ -1,4 +1,4 @@
 package module
 
-const constVersionHistoryFall string = "1.0.ae1c9e18"
+const constVersionHistoryFall string = "1.0.0f32a9df"
 const constDateUpdateHistoryFall string = "02-25-2024"
