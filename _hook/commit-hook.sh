4da70da4db664a09c73c63688972663647c31a75
SunSung W541 <mail@embedded.biz.ua>
2024-02-25 15:58:10 +0100

    clear commit

--- _hook/commit-hook.sh
+++ _hook/commit-hook.sh
@@ -1,24 +1,4 @@
 #!/bin/sh
 
-VERSION="1.0"
-fileConst="$PWD/pkg/module/historyConst.go"
-dateNow=$(date +"%m-%d-%Y")
-
-
-##################################################################################
-
-NAME=$(git branch | grep '*')
-HASH=$( (printf "commit %s\0" $(git cat-file commit HEAD | wc -c); git cat-file commit HEAD) | sha1sum )
-HASH=$(echo $HASH | cut -c -8)
-
-echo "$NAME [$VERSION.$HASH]" ': ' $(cat "$1") > "$1"
-
-###################################
-
-echo "package module" > "$fileConst"
-echo "" >> "$fileConst"
-echo "const constVersionHistoryFall string = \"$VERSION.$HASH\"" >> "$fileConst"
-echo "const constDateUpdateHistoryFall string = \"$dateNow\"" >> "$fileConst"
-
 exit 0
 
