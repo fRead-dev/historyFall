66e54cd22daa36c87c2b1ce15f9bbbb6d575f88e
SunSung W541 <mail@embedded.biz.ua>
2024-02-25 19:43:54 +0100

    * temp/db/test [1.0.0e0c6935] :  заготовка под тестирование векторов

--- pkg/module/historyConst.go
+++ pkg/module/historyConst.go
@@ -1,4 +1,4 @@
 package module
 
-const constVersionHistoryFall string = "1.0.560a566f"
+const constVersionHistoryFall string = "1.0.d595cd4f"
 const constDateUpdateHistoryFall string = "02-25-2024"
