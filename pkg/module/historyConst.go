fea72b243053afbb0ac4724eb1db6aaf21d6fa04
SunSung W541 <mail@embedded.biz.ua>
2024-02-26 01:27:02 +0100

    * temp/gln [1.0.c5952c30] :  попытка обратной генерации

--- pkg/module/historyConst.go
+++ pkg/module/historyConst.go
@@ -1,4 +1,4 @@
 package module
 
-const constVersionHistoryFall string = "1.0.aa34ac1a"
+const constVersionHistoryFall string = "1.0.4f8b6946"
 const constDateUpdateHistoryFall string = "02-26-2024"
