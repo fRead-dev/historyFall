c37a94e684022b5eb12d5befbd2f080eb13c00a6
SunSung W541 <mail@embedded.biz.ua>
2024-02-26 06:07:18 +0100

    * temp/gln [1.0.b8165c27] :  игры с двунаправленостью как то потом

--- pkg/module/historyVector.go
+++ pkg/module/historyVector.go
@@ -3,6 +3,7 @@ package module
 import (
 	"bytes"
 	"compress/flate"
+	"fmt"
 	"github.com/sergi/go-diff/diffmatchpatch"
 	"go.uber.org/zap"
 	"io/ioutil"
@@ -15,18 +16,79 @@ func (obj HistoryFallObj) DecodeStoryVector(comparison *string) []diffmatchpatch
 	return diffs
 }
 
+func countStringDuplicates(arr []string) map[string]int {
+	counts := make(map[string]int)
+
+	// Подсчитываем количество каждой строки в массиве
+	for _, value := range arr {
+		counts[value]++
+	}
+
+	return counts
+}
+
 // Получение дельты изменения, сравнивая два текста
 func (obj HistoryFallObj) generateStoryVector(oldText *[]byte, newText *[]byte) string {
 
 	//	Формирование вектора
 	dmp := diffmatchpatch.New()
-	diffs := dmp.DiffMain(string(*oldText), string(*newText), false)
-	vector := dmp.DiffToDelta(diffs)
+	diffs_1to2 := dmp.DiffMain(string(*oldText), string(*newText), false)
+	diffs_1to2 = dmp.DiffCleanupSemantic(diffs_1to2) //	Семантическое упрощение
+	vector_1to2 := dmp.DiffToDelta(diffs_1to2)
+
+	diffs_2to1 := dmp.DiffMain(string(*newText), string(*oldText), false)
+	diffs_2to1 = dmp.DiffCleanupSemantic(diffs_2to1) //	Семантическое упрощение
+	vector_2to1 := dmp.DiffToDelta(diffs_2to1)
+
+	diffs2, _ := dmp.DiffFromDelta(string(*oldText), vector_1to2)
+	text2 := dmp.DiffText2(diffs2)
+
+	diffs1, _ := dmp.DiffFromDelta(string(*newText), vector_2to1)
+	text1 := dmp.DiffText2(diffs1)
+
+	//.//
+
+	//.//
+
+	//todo сама идея - собрать не повторяющиеся записи и хранить два вектора указателями на уникальные записи c битом типа
+	var sha []string
+
+	for _, diff := range diffs1 {
+		sha = append(sha, SHA1(diff.Text))
+		//sha = append(sha, SHA1(diff.Text+strconv.Itoa(int(diff.Type))))
+	}
+	for _, diff := range diffs2 {
+		sha = append(sha, SHA1(diff.Text))
+		//sha = append(sha, SHA1(diff.Text+strconv.Itoa(int(diff.Type))))
+	}
+
+	duplicates := countStringDuplicates(sha)
+
+	// Выводим информацию о дубликатах
+	i := 0
+	non := 0
+	for value, count := range duplicates {
+		if count > 1 {
+			fmt.Printf("Строка \"%s\" встретилась %d раз(а)\n", value, count)
+			i++
+		} else {
+			non++
+		}
+	}
+	fmt.Printf("Не уникальных %d/%d из %d \n", i, non, len(sha))
+
+	obj.log.Info("newText", zap.Any("status", SHA1(text2) == SHA1(string(*newText))), zap.Any("size", len(vector_1to2)))
+	obj.log.Info("oldText", zap.Any("status", SHA1(text1) == SHA1(string(*oldText))), zap.Any("size", len(vector_2to1)))
+	obj.log.Panic("END")
+
+	//.//
+
+	//.//
 
 	//	Сжатие
 	var compressed bytes.Buffer
 	writer, _ := flate.NewWriter(&compressed, flate.BestCompression)
-	writer.Write([]byte(vector))
+	//writer.Write([]byte(vector))
 	writer.Close()
 
 	return compressed.String()
