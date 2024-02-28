1616370d701f07eb931d2774b8cc80766a4504f9
SunSung W541 <mail@embedded.biz.ua>
2024-02-28 15:53:26 +0100

    * temp/gln [1.0.941ecdc6] :  поиск по вектору

--- pkg/module/historyConst.go
+++ pkg/module/historyConst.go
@@ -1,4 +1,4 @@
 package module
 
-const constVersionHistoryFall string = "1.0.b68c2d33"
+const constVersionHistoryFall string = "1.0.62fd8ed8"
 const constDateUpdateHistoryFall string = "02-28-2024"
