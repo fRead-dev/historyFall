83826f02811a9b8373701691c707b564877d7cf2
SunSung W541 <mail@embedded.biz.ua>
2024-02-28 04:06:12 +0100

    * temp/gln [1.0.8270ef75] :  поправил привязку типов для ключа

--- pkg/module/historyConst.go
+++ pkg/module/historyConst.go
@@ -1,4 +1,4 @@
 package module
 
-const constVersionHistoryFall string = "1.0.dd2a6853"
+const constVersionHistoryFall string = "1.0.6e9c394b"
 const constDateUpdateHistoryFall string = "02-28-2024"
