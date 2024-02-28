2e944e7f0859fbef31672c0d7e36e27dd11cc270
SunSung W541 <mail@embedded.biz.ua>
2024-02-28 03:12:33 +0100

    * temp/gln [1.0.56b896e0] :  анатации

--- pkg/module/historyConst.go
+++ pkg/module/historyConst.go
@@ -1,4 +1,4 @@
 package module
 
-const constVersionHistoryFall string = "1.0.528a92ef"
+const constVersionHistoryFall string = "1.0.4d04ad2b"
 const constDateUpdateHistoryFall string = "02-28-2024"
