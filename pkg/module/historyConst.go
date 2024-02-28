42b4c06b90b0c18673f753714c79d3fef32a02db
SunSung W541 <mail@embedded.biz.ua>
2024-02-28 14:53:31 +0100

    * temp/gln [1.0.26578e2e] :  исправлена структура базы

--- pkg/module/historyConst.go
+++ pkg/module/historyConst.go
@@ -1,4 +1,4 @@
 package module
 
-const constVersionHistoryFall string = "1.0.23b682e8"
+const constVersionHistoryFall string = "1.0.1ca4cd6b"
 const constDateUpdateHistoryFall string = "02-28-2024"
