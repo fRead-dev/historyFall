4cf29d3359572fffe8dc2be83d47f93926fb469e
SunSung W541 <mail@embedded.biz.ua>
2024-02-28 15:59:14 +0100

    * temp/gln [1.0.03c85c5d] :  build_run.log _example go.mod go.sum go.work go.work.sum _hook index.go LICENSE pkg README.md system temp/gln [1.0.1616370d] : расширил кеширование хешей

--- pkg/module/historyConst.go
+++ pkg/module/historyConst.go
@@ -1,4 +1,4 @@
 package module
 
-const constVersionHistoryFall string = "1.0.62fd8ed8"
+const constVersionHistoryFall string = "1.0.1616370d"
 const constDateUpdateHistoryFall string = "02-28-2024"
