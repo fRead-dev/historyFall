34590f8ee49ef3e0e82d1c76c1fae16a98fcd2f7
SunSung W541 <mail@embedded.biz.ua>
2024-02-28 16:24:19 +0100

    * temp/gln [1.0.cc158b26] :  кеш и начало файлов

--- pkg/module/historyConst.go
+++ pkg/module/historyConst.go
@@ -1,4 +1,4 @@
 package module
 
-const constVersionHistoryFall string = "1.0.03c85c5d"
+const constVersionHistoryFall string = "1.0.4cf29d33"
 const constDateUpdateHistoryFall string = "02-28-2024"
