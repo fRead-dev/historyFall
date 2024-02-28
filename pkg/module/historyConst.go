440187961ee985bedba3a7a8dc49551eeca873df
SunSung W541 <mail@embedded.biz.ua>
2024-02-28 21:10:48 +0100

    * temp/gln [1.0.336699cc] :  таймлайн закончен?

--- pkg/module/historyConst.go
+++ pkg/module/historyConst.go
@@ -1,4 +1,4 @@
 package module
 
-const constVersionHistoryFall string = "1.0.82261902"
+const constVersionHistoryFall string = "1.0.1dead858"
 const constDateUpdateHistoryFall string = "02-28-2024"
