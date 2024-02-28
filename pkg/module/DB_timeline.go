231719754ec19a1c219ee4f4bbadc91efd23f995
SunSung W541 <mail@embedded.biz.ua>
2024-02-28 19:30:56 +0100

    * temp/gln [1.0.801bc884] :  получение таймточки

--- pkg/module/DB_timeline.go
+++ pkg/module/DB_timeline.go
@@ -1,14 +1,45 @@
 package module
 
+import (
+	"database/sql"
+	"errors"
+	"go.uber.org/zap"
+)
+
 // /	#############################################################################################	///
 type _historyFall_dbTimeline struct {
 	globalObj *localSQLiteObj
 }
 
-/*	Получение точки истории по ID	*/
-func (obj *_historyFall_dbTimeline) Get(id uint32) (_historyFall_dbTimeline, bool) {
-	retObj := _historyFall_dbTimeline{}
+/*	Получение точки истории по ID */
+func (obj *_historyFall_dbTimeline) Get(id uint32) (database_hf_timeline, bool) {
+	retObj := database_hf_timeline{}
 	status := true
+
+	//	Поиск по базе
+	err := obj.globalObj.db.QueryRow("SELECT `id`, `ver`, `time`, `file`, `vector` FROM `database_hf_timeline` WHERE `id` = ?", id).Scan(
+		&retObj.ID,
+		&retObj.Ver,
+		&retObj.Time,
+		&retObj.File.ID,
+		&retObj.Vector.Info.ID,
+	)
+	if err != nil {
+		if !errors.Is(err, sql.ErrNoRows) { //Обработка если ошибка не связана с пустым значением
+			obj.globalObj.log.Error("DB", zap.String("func", "Timeline:Get"), zap.Error(err))
+		}
+		status = false
+	}
+
+	//	Загружаем вектор
+	if status {
+		retObj.Vector, status = obj.globalObj.Vector.Get(retObj.Vector.Info.ID)
+	}
+
+	//	Загружаем файл
+	if status {
+		retObj.File, status = obj.globalObj.File.Get(retObj.File.ID)
+	}
 
 	return retObj, status
 }
--- pkg/module/historyConst.go
+++ pkg/module/historyConst.go
@@ -1,4 +1,4 @@
 package module
 
-const constVersionHistoryFall string = "1.0.05abd549"
+const constVersionHistoryFall string = "1.0.9377dad4"
 const constDateUpdateHistoryFall string = "02-28-2024"
