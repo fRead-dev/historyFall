1cf41c1987f1acc09fb89c543c8291d12c6a70ce
SunSung W541 <mail@embedded.biz.ua>
2024-02-29 21:46:47 +0100

    * main [1.0.af6c9abd] :  да похер на тот sum

--- pkg/module/historyConst.go
+++ pkg/module/historyConst.go
@@ -1,4 +1,4 @@
 package module
 
-const constVersionHistoryFall string = "1.0.a7deb233"
+const constVersionHistoryFall string = "1.0.d544d3d0"
 const constDateUpdateHistoryFall string = "02-29-2024"
