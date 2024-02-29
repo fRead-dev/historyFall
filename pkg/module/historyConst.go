14fd48e69c73ce83a9fa1abb92e3e6e82e065473
SunSung W541 <mail@embedded.biz.ua>
2024-02-29 02:10:22 +0100

    * temp/test [1.0.11181033] :  тап времени переведен на милисекунды

--- pkg/module/historyConst.go
+++ pkg/module/historyConst.go
@@ -1,4 +1,4 @@
 package module
 
-const constVersionHistoryFall string = "1.0.41662af8"
+const constVersionHistoryFall string = "1.0.d66114c1"
 const constDateUpdateHistoryFall string = "02-29-2024"
