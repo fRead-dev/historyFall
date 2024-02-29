2ef0c5ec6853476871cc18a0e7b9e1ce6c4a990e
SunSung W541 <mail@embedded.biz.ua>
2024-02-29 22:28:36 +0100

    2

--- pkg/module/historyConst.go
+++ pkg/module/historyConst.go
@@ -1,4 +1,4 @@
 package module
 
-const constVersionHistoryFall string = "1.0.85689107"
+const constVersionHistoryFall string = "1.0.c42d058d"
 const constDateUpdateHistoryFall string = "02-29-2024"
