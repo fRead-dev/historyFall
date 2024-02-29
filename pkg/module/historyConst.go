33b7b2832fac8306b07844495670cc40d997da7d
SunSung W541 <mail@embedded.biz.ua>
2024-02-29 23:44:54 +0100

    WIN

--- pkg/module/historyConst.go
+++ pkg/module/historyConst.go
@@ -1,4 +1,4 @@
 package module
 
-const constVersionHistoryFall string = "1.0.2ef0c5ec"
+const constVersionHistoryFall string = "1.0.42fc05da"
 const constDateUpdateHistoryFall string = "02-29-2024"
